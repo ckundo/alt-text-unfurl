41c6be5cce077acc23c77dc02f3ea6cb70f275b3
Cameron Cundiff <cameron@ckundo.com>
2021-06-16 20:34:44 -0400

    Add twitter fetch and auth

--- app.js
+++ app.js
@@ -1,20 +1,11 @@
 const { App, ExpressReceiver } = require('@slack/bolt');
-const Twitter = require('twitter-v2');
+const Twitter = require('twitter');
 const serverlessExpress = require('@vendia/serverless-express');
 
 const expressReceiver = new ExpressReceiver({
   signingSecret: process.env.SLACK_SIGNING_SECRET,
-  // The `processBeforeResponse` option is required for all FaaS environments.
-  // It allows Bolt methods (e.g. `app.message`) to handle a Slack request
-  // before the Bolt framework responds to the request (e.g. `ack()`). This is
-  // important because FaaS immediately terminate handlers after the response.
   processBeforeResponse: true
 });
-
-// const client = new Twitter({
-//   consumer_key: process.env.TWITTER_KEY,
-//   consumer_secret: process.env.TWITTER_SECRET,
-// });
 
 const app = new App({
   signingSecret: process.env.SLACK_SIGNING_SECRET,
@@ -22,21 +13,37 @@ const app = new App({
   receiver: expressReceiver,
 });
 
+const twitter = new Twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+});
+
 app.event('link_shared', async ({ event, client, channel }) => {
   const url = event.links[0].url;
   const parts = url.split("/")
-  // const id = parts[parts.length - 1];
+  const id = parts[parts.length - 1];
 
-  // const { data } = await client.get('tweets', {
-  //   id,
-  //   expansions: 'attachments.media_keys'
-  // });
 
-  const unfurls = {}
+  try {
+    const result = await twitter.get('statuses/show', { id: id, include_ext_alt_text: true });
+    const unfurls = {};
+    let descriptions = [];
 
-  unfurls[url] = { title: "something" }
+    if (!result.extended_entities) {
+      descriptions = ["could not find alt text."];
+    } else {
+      descriptions = result.extended_entities.media.map(img => img.ext_alt_text);
+    }
 
-  client.chat.unfurl({ token: process.env.SLACK_BOT_TOKEN, ts: event.message_ts, channel: event.channel, unfurls });
+    unfurls[url] = { title: descriptions.join("\n") }
+    client.chat.unfurl({ token: process.env.SLACK_BOT_TOKEN, ts: event.message_ts, channel: event.channel, unfurls });
+    console.log(descriptions);
+  } catch (err) {
+    console.log(err);
+  }
+
 });
 
 module.exports.handler = serverlessExpress({
